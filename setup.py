9bab6a0bb210bd572b5e4efa9b3a59551e409c5f
Aikhjarto <wagner-thomas@gmx.at>
2024-03-05 12:06:38 +0100

    fix missing stuff in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(name='lmdb_sensor_storage',
       version='0.1',
@@ -9,6 +10,18 @@ setup(name='lmdb_sensor_storage',
       author_email='wagner-thomas@gmx.at',
       url='https://github.com/Aikhjarto/lmdb_sensor_storage',
       scripts=['scripts/check_lmdb_sensor_data_age.py'],
+      packages=find_packages(where='src'),
+      package_dir={"": "src"},
+      install_requires=["lmdb",
+                        "numpy>=1.7",
+                        "plotly>=4.0",
+                        "urllib3",
+                        "paho-mqtt",
+                        "PyYAML",
+                        "requests",
+                        "regex",
+                        "typing_extensions",
+                        ],
       include_package_data=True,
       package_data={'lmdb_sensor_storage': ['**/*.service', '**/*.timer'], }
       )
